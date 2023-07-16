e6e7532c111a4135dcf9461ec4bcc424583ff773
Kyel Governor <k.gov@hotmail.com>
2023-07-16 13:42:45 +0000

    srn

--- src/MLP.py
+++ src/MLP.py
@@ -107,3 +107,5 @@ print ("0 0 = {0:.10f}".format(mlp.run([0,0])[0]))
 print ("0 1 = {0:.10f}".format(mlp.run([0,1])[0]))
 print ("1 0 = {0:.10f}".format(mlp.run([1,0])[0]))
 print ("1 1 = {0:.10f}".format(mlp.run([1,1])[0]))
+
+
